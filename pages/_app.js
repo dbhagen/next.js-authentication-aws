e5c9c5b2f470251f1ccd04c1f29cf07ad4c3de8c
Daniel Hagen <daniel.b.hagen@gmail.com>
2020-11-17 12:56:41 -0600

    Navbar signout

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,19 @@ import '../styles/globals.css';
 import Link from 'next/link';
 import { css } from 'emotion';
 
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import config from '../src/aws-exports';
 Amplify.configure({
   ...config,
   ssr: true
 });
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp ({ Component, pageProps }) {
+  const doSignOutNav = () => {
+    Auth.signOut()
+      .then(data => console.log('Then Data:',data))
+      .catch(err => console.log('Catch Error:', err));
+  }
   return (
     <div>
       <nav className={navStyle}>
@@ -25,6 +30,9 @@ export default function MyApp({ Component, pageProps }) {
         <Link href="/protected-client">
           <span className={linkStyle}>Protected Client</span>
         </Link>
+        <button onClick={doSignOutNav}>
+          Sign Out
+        </button>
       </nav>
       <Component {...pageProps} />
     </div>
